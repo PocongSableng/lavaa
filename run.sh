aeaef1da240512884106c85df552fad31aee350d
Pocong Sableng <35644233+PocongSableng@users.noreply.github.com>
2024-03-28 16:30:08 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-8e7b875c-cacb-4de9-bcf8-c7c979cf5912/)
 
 
 echo "$curl_output" >> Update.tsx
